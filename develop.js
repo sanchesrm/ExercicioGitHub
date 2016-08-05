63715b6723cc1009c6e8c307ccaf8c23ddb17127
sanchesrm <sanchesrm@hotmail.com>
2016-08-05 11:49:51 -0300

    First file commited

---
+++ develop.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000, function () {
+  console.log('This is the first exercise of ExercicioGitHub. Server is now running on port 3000!');
+});
+
+app.get('/', function (req, res) {
+  res.send('This is the first exercise of ExercícioGitHub.');
+});
+
+
+
